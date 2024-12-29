b9d310161e8f083a20eb83188ebb769825f82cdd
Herika <herikarajput3@gmail.com>
2024-12-29 11:32:52 +0530

    first commit

---
+++ 1. Weather App/script.js
@@ -0,0 +1,6 @@
+const APIKey = c3fdd4e2e28b61bc5e60102420e00c3d;
+const APIURL = "https://api.openweathermap.org/data/2.5/weather?units=metrics&q=bangalore";
+
+async function checkWeather() {
+    
+}
