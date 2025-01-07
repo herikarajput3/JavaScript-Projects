82d231bf84f91c82eb7e3e207578e930ede975d2
Herika <herikarajput3@gmail.com>
2025-01-07 17:36:06 +0530

    Just added check uncheck task using javascript

--- 2. To-do List/script.js
+++ 2. To-do List/script.js
@@ -3,6 +3,7 @@ let input = document.querySelector("#taskInput");
 let button = document.querySelector(".addBtn");
 let allTasksList = document.querySelector("#allTasksList");
 
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -23,29 +24,28 @@ function show() {
     let taskData = JSON.parse(localStorage.getItem("taskData"));
     let result = "";
 
-    taskData.map((items) => {
+    taskData.map((items, index) => {
         result += `
         <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
-                                            style="background-color: #f4f6f7;" onclick="toggle(this)">    
-            <input class="form-check-input me-2" type="checkbox" value="" aria-label="..." />
-                                            ${items.task}
+                                            style="background-color: #f4f6f7;" data-index = "${index}">   
+                    <input class="form-check-input me-2" type="checkbox" onchange=toggle(this) />
+                            ${items.task}
         </li>`
     })
 
     allTasksList.innerHTML = result;
 }
 
-function toggle(element) {
-    let checkbox = document.querySelector(".form-check-input");
+function toggle(checkbox) {
+    const listItem = checkbox.closest(".list-group-item");
+    // const listItem = document.querySelector(".list-group-item"); // It will not work because it selects only 1st task.
+    // We have to select multiple task so that we use closest to select task 
+
     if (checkbox.checked) {
-        element.style.textDecorationLine = "line-through";
+        listItem.style.textDecoration = "line-through";
     } else {
-        element.style.textDecorationLine = "none";
+        listItem.style.textDecoration = "none";
     }
-
-    // element.checked = true;
-    // element.checked = false;
-
 }
 
 
