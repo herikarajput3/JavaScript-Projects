3a8cbf8563e2b98305e04a7f51062ff40713599d
Herika <herikarajput3@gmail.com>
2025-01-12 17:40:52 +0530

    Added dynamic dropdown list menu using javascript for selection user.

--- 4. Task Management/js/script.js
+++ 4. Task Management/js/script.js
@@ -3,8 +3,20 @@ let taskName = document.querySelector(".taskName");
 let taskDesc = document.querySelector(".taskDesc");
 let taskStatus = document.querySelector("#taskStatus");
 let user = document.querySelector("#user");
-// let saveBtn = document.querySelector(".saveBtn");
-// let taskID = null;
+
+function selectUser() {
+    let user_list = JSON.parse(localStorage.getItem("user_list")) || [];
+    user.innerHTML = `<option selected>Select Employee</option>`;
+
+    user_list.map((items) => {
+        const option = document.createElement("option");
+        option.value = items.user_name;
+        option.textContent = items.user_name;
+        user.appendChild(option);
+    });
+}
+
+selectUser();
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -18,7 +30,6 @@ form.addEventListener("submit", (e) => {
     let taskList = JSON.parse(localStorage.getItem("taskList")) || [];
 
     taskList.push(taskDetail);
-
 
     localStorage.setItem("taskList", JSON.stringify(taskList));
 
