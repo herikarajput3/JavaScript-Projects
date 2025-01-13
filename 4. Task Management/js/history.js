f9495f868338c52b3dbfc4692b850a9e9ff24cdf
Herika <herikarajput3@gmail.com>
2025-01-13 09:10:06 +0530

    History page design

---
+++ 4. Task Management/js/history.js
@@ -0,0 +1,25 @@
+tbody = document.querySelector("tbody");
+
+function displayTaskHistory() {
+    let taskHistory = JSON.parse(localStorage.getItem("taskListHistory")) || [];
+    let taskList = "";
+
+    taskHistory.map((items, index) => {
+        taskList +=
+            `
+        <tr>
+            <th scope="row">${index+1}</th>
+            <td>${items.taskName}</td>
+            <td>${items.taskDesc}</td>
+            <td>${items.taskStatus}</td>
+            <td>${items.user}</td>
+            <td>
+                <button class="btn btn-danger mb-4 mb-lg-0" onclick="removeTask(${index})">Remove</button>
+                <button class="btn btn-success" onclick="restoreTask(${index})">Restore</button>
+            </td>
+        </tr>
+        `
+        tbody.innerHTML = taskList;
+    })
+}
+displayTaskHistory();
