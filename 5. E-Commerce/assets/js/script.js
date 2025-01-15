408b4724a9059377e74742cfd31167e1ed93d000
Herika <herikarajput3@gmail.com>
2025-01-15 16:47:24 +0530

    Added skeleton for loading animation and added cart page

--- 5. E-Commerce/assets/js/script.js
+++ 5. E-Commerce/assets/js/script.js
@@ -1,32 +1,60 @@
 let productRow = document.querySelector("#productRow");
 const API_URL = "https://fakestoreapi.com/products";
+let cartItems = document.querySelector("#cartItems");
 
 
-fetch(API_URL)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+const fetchProducts = async () => {
+    try {
+        // loader
+        let skeleton = `
+        <div class="col-md-3 mb-4">
+            <div class="border rounded p-3 d-flex flex-column">
+                <div class="product-image bg-body-secondary rounded" style="aspect-ratio: 1;"></div>
+                    <div class="p-3 d-flex flex-column flex-grow-1">
+                        <div class="product-title h-4 bg-body-secondary rounded w-75"></div>
+                        <div class="card-text h-4 bg-body-secondary rounded w-100 mt-2"></div>
+                        <div class="product-price h-4 bg-body-secondary rounded w-50 mt-2"></div>
+                        <div class="h-10 bg-body-secondary rounded mt-2"></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        `;
+        productRow.innerHTML = skeleton.repeat(8);
+
+        // fetch data from URL
+        const data = await fetch(API_URL);
+
+        if (!data.ok) {
+            console.log(`Error!! status: ${data.status}`);
         }
-        return response.json();
-    })
-    .then(products => {
-        productRow.innerHTML = "";
-        let productCard = "";
 
+        const products = await data.json();
+        let productCard = "";
+        productRow.innerHTML = "";
         products.map(product => {
             productCard += `
             <div class="col-md-3 mb-4">
-                <div class="product-card">
-                <img src="${product.image}" class="product-image img-fluid" alt="${product.title}">
-                    <div class="p-3">
+                <div class="product-card border rounded p-3 d-flex flex-column">
+                    <img src="${product.image}" class="product-image img-fluid rounded" alt="${product.title}">
+                    <div class="p-3 d-flex flex-column flex-grow-1">
                         <h5 class="product-title">${product.title}</h5>
-                        <p class="card-text">${product.description.substring(0, 100)}...</p>
-                        <p class="product-price"> &dollar;${product.price}</p>
-                        <button class="btn btn-primary">Add to Cart</button>
+                        <p class="card-text">${product.description}</p>
+                        <p class="product-price fw-bold">Price: &dollar;${product.price}</p>
+                        <button class="btn btn-primary mt-auto ">Add to Cart</button>
                     </div>
                 </div>
-            </div>`
+            </div>`;
 
             productRow.innerHTML = productCard;
         });
-    })
+
+    } catch (error) {
+        productRow.innerHTML = `<p class="text-danger">Failed to load products. Please try again later.</p>`;
+        console.error("Error fetching products:", error);
+    }
+}
+
+fetchProducts();
+
+
