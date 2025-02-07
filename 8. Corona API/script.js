547f67ca6adb0c31193a9f2df48aea32d8d35a8c
Herika <herikarajput3@gmail.com>
2025-02-07 18:01:19 +0530

    Completed Corona api project

--- 8. Corona API/script.js
+++ 8. Corona API/script.js
@@ -3,11 +3,30 @@ const url = "https://api.rootnet.in/covid19-in/stats/latest";
 async function coronaAPI() {
     const result = await fetch(url);
     const json = await result.json();
-    const data = await json.data;
-    const regional_data = await data.regional;
-
+    const json_data = await json.data;
+    const regional_data = await json_data.regional;
+    let tbody = document.querySelector(".tbody")
     // console.log(regional_data);
+    // regional_data.forEach(element => {
+    //     console.log(element.loc);
 
+    // });
+
+    let data = "";
+    regional_data.map((item, index) => {
+        // console.log(item);
+        data +=
+            `
+            <tr>
+                <th scope="row">${index + 1}</th>
+                    <td>${item.loc}</td>
+                    <td>${item.discharged}</td>
+                    <td>${item.deaths}</td>
+                    <td>${item.confirmedCasesIndian}</td>
+                </tr>
+            `
+    })
+    tbody.innerHTML = data;
 }
 
 coronaAPI()
