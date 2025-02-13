400aa5ae9e98417b1dc4e6b7ca5241aee08bbbe3
Herika <herikarajput3@gmail.com>
2025-02-13 16:50:41 +0530

    Practicing to do List

--- 2. To-do List Using Local Stroage/script.js
+++ 2. To-do List Using Local Stroage/script.js
@@ -9,77 +9,69 @@ let activeTasksList = document.querySelector("#activeTasksList");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    // const task = {
-    //     task: input.value
-    // }
-
-    const task = input.value.trim(); // Get the task value
-
+    const task = input.value;
     let taskData = JSON.parse(localStorage.getItem("taskData")) || [];
     taskData.push({ task, completed: false });
     localStorage.setItem("taskData", JSON.stringify(taskData));
-
     input.value = "";
-    show()
-
 })
 
-function show() {
+// function show() {
 
-    let taskData = JSON.parse(localStorage.getItem("taskData")) || [];
+//     let taskData = JSON.parse(localStorage.getItem("taskData")) || [];
 
-    let allTasks = "";
-    let activeTasks = "";
-    let completedTasks = "";
+//     let allTasks = "";
+//     let activeTasks = "";
+//     let completedTasks = "";
 
-    taskData.map((item, index) => {
-        // allTasks += `
-        // <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
-        //                                     style="background-color: #f4f6f7;" data-index = "${index}">   
-        //             <input class="form-check-input me-2" type="checkbox" onchange=toggle(this) />
-        //                     ${items.task}
-        // </li>`
-        allTasks += `
-        <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
-            style="background-color: #f4f6f7;" data-index="${index}">
-            <input class="form-check-input me-2" type="checkbox" ${item.completed ? "checked" : ""} 
-                onchange="toggle(${index})">
-            <span style="${item.completed ? "text-decoration: line-through;" : ""}">${item.task}</span>
+//     taskData.map((item, index) => {
+//         // allTasks += `
+//         // <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
+//         //                                     style="background-color: #f4f6f7;" data-index = "${index}">
+//         //             <input class="form-check-input me-2" type="checkbox" onchange=toggle(this) />
+//         //                     ${items.task}
+//         // </li>`
+//         allTasks += `
+//         <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
+//             style="background-color: #f4f6f7;" data-index="${index}">
+//             <input class="form-check-input me-2" type="checkbox" ${item.completed ? "checked" : ""}
+//                 onchange="toggle(${index})">
+//             <span style="${item.completed ? "text-decoration: line-through;" : ""}">${item.task}</span>
 
-            <i class="fa-regular fa-circle-xmark ms-auto text-danger" style="cursor: pointer;" onclick="removeTask(${index})"></i>
-        </li>`
+//             <i class="fa-regular fa-circle-xmark ms-auto text-danger" style="cursor: pointer;" onclick="removeTask(${index})"></i>
+//         </li>`
 
-        if (!item.completed) {
-            activeTasks += `
-        <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
-            style="background-color: #f4f6f7;">
-            <input class="form-check-input me-2" type="checkbox" 
-                onchange="toggle(${index})">
-            <span>${item.task}</span>
-        </li>`;
-        }
+//         if (!item.completed) {
+//             activeTasks += `
+//         <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
+//             style="background-color: #f4f6f7;">
+//             <input class="form-check-input me-2" type="checkbox"
+//                 onchange="toggle(${index})">
+//             <span>${item.task}</span>
+//         </li>`;
+//         }
 
-        if (item.completed) {
-            completedTasks += `
-        <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
-            style="background-color: #f4f6f7;>
-            <input class="form-check-input me-2" type="checkbox" checked 
-                onchange="toggle(${index})">
-            <span style="text-decoration: line-through;">${item.task}</span>
-        </li>`;
-        }
-    });
+//         if (item.completed) {
+//             completedTasks += `
+//         <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded"
+//             style="background-color: #f4f6f7;>
+//             <input class="form-check-input me-2" type="checkbox" checked
+//                 onchange="toggle(${index})">
+//             <span style="text-decoration: line-through;">${item.task}</span>
+//         </li>`;
+//         }
+//     });
 
-    allTasksList.innerHTML = allTasks;
-    activeTasksList.innerHTML = activeTasks;
-    completedTasksList.innerHTML = completedTasks;
+//     allTasksList.innerHTML = allTasks;
+//     activeTasksList.innerHTML = activeTasks;
+//     completedTasksList.innerHTML = completedTasks;
 
-}
+// }
 
 // function toggle(checkbox) {
 //     const listItem = checkbox.closest(".list-group-item");
 //     // const listItem = document.querySelector(".list-group-item"); // It will not work because it selects only 1st task.
-//     // We have to select multiple task so that we use closest to select task 
+//     // We have to select multiple task so that we use closest to select task
 
 //     if (checkbox.checked) {
 //         listItem.style.textDecoration = "line-through";
@@ -89,19 +81,19 @@ function show() {
 //     }
 // }
 
-function toggle(index) {
-    let taskData = JSON.parse(localStorage.getItem("taskData")); // Get tasks from storage
-    taskData[index].completed = !taskData[index].completed;
-    localStorage.setItem("taskData", JSON.stringify(taskData)); // Save changes
-    show(); 
-}
+// function toggle(index) {
+//     let taskData = JSON.parse(localStorage.getItem("taskData")); // Get tasks from storage
+//     taskData[index].completed = !taskData[index].completed;
+//     localStorage.setItem("taskData", JSON.stringify(taskData)); // Save changes
+//     show();
+// }
 
-function removeTask(index) {
-    let taskData = JSON.parse(localStorage.getItem("taskData")) || [];
-    taskData.splice(index, 1);
-    localStorage.setItem("taskData", JSON.stringify(taskData));
-    show();
-}
+// function removeTask(index) {
+//     let taskData = JSON.parse(localStorage.getItem("taskData")) || [];
+//     taskData.splice(index, 1);
+//     localStorage.setItem("taskData", JSON.stringify(taskData));
+//     show();
+// }
 
 
 
